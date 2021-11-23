2db0fbe25e172eb4c14f30116e737e83d8066ec1
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 11:46:51 +0530

    Added:Sweet Qualities

---
+++ src/com/workshop/Mothichur.java
@@ -0,0 +1,15 @@
+package com.workshop;
+import java.util.ArrayList;
+
+public class Mothichur extends Sweet {
+	
+	public Mothichur() {
+		shape = "round";
+		colour = "red";
+		ingredient =new ArrayList();
+		ingredient.add("sugar");
+		ingredient.add("channa flour");
+		ingredient.add("ghee");
+		name = "Mothichur";
+	}
+}
