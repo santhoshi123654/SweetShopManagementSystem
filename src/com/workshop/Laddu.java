9d1a094604b1cdc23511f8a267344a9fe9a144d5
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 11:44:34 +0530

    Added:Sweet Qualities

---
+++ src/com/workshop/Laddu.java
@@ -0,0 +1,17 @@
+package com.workshop;
+
+	import java.util.ArrayList;
+
+	public class Laddu extends Sweet {
+		
+		public Laddu() {
+			shape = "round";
+			colour = "yellow";
+			ingredient =new ArrayList();
+			ingredient.add("sugar");
+			ingredient.add("channa flour");
+			ingredient.add("ghee");
+			name = "Laddu";
+		}
+		
+	}
