554f14eec0baf50f52ec6963e004478aeb2fe546
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 11:43:49 +0530

    Added:KajuBarfi Qualities

---
+++ src/com/workshop/KajuBarfi.java
@@ -0,0 +1,16 @@
+package com.workshop;
+
+import java.util.ArrayList;
+
+public class KajuBarfi extends Sweet{
+	
+	public KajuBarfi() {
+		shape = "diamond";
+		colour = "white";
+		ingredient =new ArrayList();
+		ingredient.add("sugar");
+		ingredient.add("kaju");
+		ingredient.add("ghee");
+		name = "KajuBarfi";
+	}
+}
