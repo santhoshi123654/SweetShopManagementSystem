fc1309892990fd517beeada993e23d1a21b5cbf8
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-23 11:46:15 +0530

    Added:Sweet Qualities

---
+++ src/com/workshop/Modak.java
@@ -0,0 +1,17 @@
+package com.workshop;
+
+import java.util.ArrayList;
+
+public class Modak extends Sweet{
+	
+	public Modak() {
+		shape = "cone";
+		colour = "white";
+		ingredient =new ArrayList();
+		ingredient.add("sugar");
+		ingredient.add("channa dal");
+		ingredient.add("wheat flour");
+		ingredient.add("ghee");
+		name = "Modak" ;
+	}
+}
