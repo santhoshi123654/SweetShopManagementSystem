a2420e40ed487a4952a13e2ac0046890f0adf00d
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-14 10:50:23 +0530

    Added:Refactored code Using Collections

--- src/com/workshop/Application.java
+++ src/com/workshop/Application.java
@@ -1,12 +1,15 @@
 package com.workshop;
 
-	import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 	public class Application {
 		
 		final static int exitValue = 7;
-		UserInterface userInterface =new UserInterface();
-		SweetStore sweetStore = new SweetStore();
+		UserInterface userInterface = UserInterface.getInstance();
+		Scanner sc = new Scanner(System.in);
 		
 		public static void main(String[] args) {
 			System.out.println("Welcome to Sweet Shop");	
@@ -23,28 +26,15 @@ package com.workshop;
 		}
 		
 		private void handleUserSelection(int input) { 
-			UserInterface userInterface = new UserInterface();
+			UserInterface userInterface =UserInterface.getInstance();
+			SweetStore sweetStore = SweetStore.getInstance();
 			
 			switch(input) {
+			
 				case 1:
-					Laddu laddu = new Laddu();
-					laddu.price = 200;
-					Modak modak = new Modak();
-					modak.price = 300;
-					Mothichur mothichur = new Mothichur();
-					mothichur.price = 600;
-					Mothichur mothichur2 = new Mothichur();
-					mothichur2.price = 400;
-					KajuBarfi kajubarfi = new KajuBarfi();
-					kajubarfi.price = 500;
-				
-					sweetStore.add(laddu);
-					sweetStore.add(kajubarfi);
-					sweetStore.add(mothichur);
-					sweetStore.add(modak);
-					sweetStore.add(mothichur);
-					sweetStore.add(mothichur2);
+					addSweet();
 					break;
+					
 				case 2:
 					System.out.println("Enter the name you want to delete :");
 					Scanner sc = new Scanner(System.in);
@@ -52,12 +42,188 @@ package com.workshop;
 					
 					Sweet name = sweetStore.getSweet(sweetName);
 					sweetStore.removeSweet(name);
+					
+				case 3:
+					System.out.println("Enter the Sweet name you want to Update :");
+					Scanner scanner = new Scanner(System.in);
+					String sweetName1 = scanner.next();
+					Sweet sweet = sweetStore.getSweet(sweetName1);
+					updateSweet(sweet);
+					
 				case 4:
 					userInterface.printAllSweet(sweetStore.getList());
 					break;
-				case exitValue:
-					int ext;
+				case 5:
+					UserInterface userInterface1 = UserInterface.getInstance();
+					userInterface1.printAllYellowSweet(sweetStore.getList());
 					break;
-				}			
+				case 6:
+					getOrder();
+					
+					break;
+				case exitValue:
+					int exit;
+					break;
+				}		
+			
 		}
-	}
+		private void getOrder() {
+				SweetStore sweetStore = SweetStore.getInstance();	
+				Order order = new Order();
+				Scanner sc= new Scanner(System.in);
+				int quanity =0;
+				System.out.println("Welcome to SweetShop");
+				System.out.println("Enter your name :");
+				String customerName = sc.next();
+				System.out.println("Enter Your Mobile Number ");
+				long mobileNumber = sc.nextLong();
+				order.setMobileNumber(mobileNumber);
+				System.out.println("Enter the Id:");
+				String id = sc.next();
+				order.setId(id);
+				int price = 0;
+				String sweetName = null;
+				do {
+					System.out.println("Enter the Sweet Name you want to order:");
+					sweetName = sc.next();
+					Sweet sweet = sweetStore.getSweet(sweetName);	
+					if(sweetName.compareToIgnoreCase("quit")== 0) {
+						break;
+					}
+					
+					System.out.println("Enter the Quantity :");
+					int quantity = sc.nextInt();
+					price += sweet.getPrice()*quantity;
+
+					Map<Sweet, Integer> orderMap = new HashMap<Sweet,Integer>();
+					orderMap.put(sweet, quantity);
+					order.setSweetQuantityMap(orderMap);
+		
+				}while(true);
+				
+				order.setTotalPrice(price);
+				System.out.println(order);
+	 }		
+
+		private void addSweet() {
+			SweetStore store = SweetStore.getInstance();
+			Sweet sweet = new Sweet();
+			System.out.println("Enter sweet name");
+			sweet.name = sc.next();
+			System.out.println("Enter color");
+			setColour(sweet);
+			System.out.println("Enter shape");
+			setShape(sweet);
+			System.out.println("Enter price");
+			sweet.price = sc.nextInt();
+			setIngredient(sweet);
+			store.add(sweet);
+		}
+
+		private void setIngredient(Sweet sweet) {
+			System.out.println("Enter the new Ingredients sweet\",\"");
+			String newIngredients = sc.next();
+			String[] ingredientArr = newIngredients.split(",");
+
+			sweet.ingredient = Arrays.asList(ingredientArr);
+		}
+				
+		private void updatePrice(Sweet sweet) {
+			System.out.println("Enter Price");
+			int newPrice = sc.nextInt();
+			sweet.price = newPrice;
+		}
+
+		private void updateName(Sweet sweet) {
+			System.out.println("Enter Name");
+			String newName = sc.nextLine();
+			sweet.name = newName;
+		}
+
+		public void setShape(Sweet sweet) {	
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter\n1.TRIANGLE \n2.CONE \n3.ROUND \n4.DIAMOND");
+			int shape =scanner.nextInt();
+			
+			switch(shape) {
+			
+			case 1:
+				sweet.shape = Sweet.Shape.TRIANGLE;
+				break;
+			case 2:
+				sweet.shape = Sweet.Shape.CONE;
+				break;
+			case 3:
+				sweet.shape = Sweet.Shape.ROUND;
+				break;
+			case 4:
+				sweet.shape = Sweet.Shape.DIAMOND;
+				break;
+			}
+			
+		}
+		
+		public void setColour(Sweet sweet) {
+			Scanner scanner =new Scanner(System.in);
+			System.out.println("Enter\n1.RED \n2.YELLOW \n3.WHITE \n4.BROWN");
+			int colour =scanner.nextInt();
+			
+			switch(colour) {
+			
+			case 1:
+				sweet.colour = Sweet.Colour.RED;
+				break;
+			case 2:
+				sweet.colour = Sweet.Colour.YELLOW;
+				break;
+			case 3:
+				sweet.colour = Sweet.Colour.WHITE;
+				break;
+			case 4:
+				sweet.colour = Sweet.Colour.BROWN;
+				break;
+			}
+		}
+		
+//		public void updateSweetIngredients(Sweet sweet) {
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.println("Enter the Ingredients to be Updated :");
+//			String newIngredients = scanner.nextLine();
+//			sweet.ingredient.clear();
+//			String[] newIngredientsArr = newIngredients.split(" ");
+//			sweet.ingredient = Arrays.asList(newIngredientsArr);
+//		}
+		
+		public void updateSweet(Sweet sweet) {
+		
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter the thing what to be updated \n1.SweetName"
+							+ " \n2.Colour \n3.Shape \n4.Price \n5.Ingredients");
+			int change = scanner.nextInt();
+			
+			switch(change) {
+				
+			case 1:
+				setShape(sweet);
+				break;
+			case 2:
+				setColour(sweet);
+				break;
+			case 3:
+				updateName(sweet);
+				break;
+			case 4:
+				updatePrice(sweet);
+				break;
+			case 5:
+				setIngredient(sweet);
+				break;
+	
+			default:
+				System.exit(0);
+			}
+			
+		}
+			
+	}	
+
