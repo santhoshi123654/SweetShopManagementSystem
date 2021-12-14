3fff1323bb19ff26cb8fe6746f5ee0bf52174fc6
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-14 10:51:56 +0530

    Added:Sweet logic.

--- src/com/workshop/Sweet.java
+++ src/com/workshop/Sweet.java
@@ -5,12 +5,67 @@ import java.util.Objects;
 
 public class Sweet {
 	
-		String shape;
-		String colour;	
+	enum Shape {TRIANGLE, CONE, ROUND, DIAMOND}
+	enum Colour {YELLOW, RED, WHITE,BROWN}
+	
+		Shape shape;
+		Colour colour;	
 		int price;
 		List<String> ingredient;
 		String name;
-				
+		
+		@Override
+		public String toString() {
+			return "Sweet [shape=" + shape + ", colour=" + colour + ", price=" + price + ", ingredient=" + ingredient
+					+ ", name=" + name + "]";
+		}		
+		
+		public Shape getShape() {
+			return shape;
+		}
+
+		public void setShape(Shape shape) {
+			this.shape = shape;
+		}
+
+		public Colour getColour() {
+			return colour;
+		}
+
+		public void setColour(Colour colour) {
+			this.colour = colour;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+		public List<String> getIngredient() {
+			return ingredient;
+		}
+
+		public void setIngredient(List<String> ingredient) {
+			this.ingredient = ingredient;
+		}
+
+
+
+		public String getName() {
+			return name;
+		}
+
+
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+
+
 		@Override
 		public int hashCode() {
 			return Objects.hash(colour, ingredient, name, price, shape);
@@ -27,13 +82,6 @@ public class Sweet {
 			Sweet other = (Sweet) obj;
 			return Objects.equals(colour, other.colour) && Objects.equals(ingredient, other.ingredient)
 					&& Objects.equals(name, other.name) && price == other.price && Objects.equals(shape, other.shape);
-		}
-
-		@Override
-		public String toString() {
-			return getClass().getSimpleName() + " [shape=" + shape + ", colour=" + colour + ", "
-					+ "price=" + price + ", ingredient=" + ingredient + "]";
-		}
-		
+		}				
 	}
 
