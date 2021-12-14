9ce0c83e181ded0b004f8330116d834c055e23bb
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-14 10:52:29 +0530

    Added:Refactored the code.

--- src/com/workshop/SweetStore.java
+++ src/com/workshop/SweetStore.java
@@ -2,12 +2,24 @@ package com.workshop;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class SweetStore {
-
+	private static SweetStore instance;
 	private Set<Sweet> sweetList = new HashSet<>();
+	
+		private SweetStore() {
+			
+		}
 		
+		public static SweetStore getInstance() {
+			if(instance == null) {
+				instance = new SweetStore();
+			}
+			return instance;
+		}
+
 		public void add(Sweet sweet) {
 			sweetList.add(sweet);
 		}
@@ -22,11 +34,11 @@ public class SweetStore {
 		
 		public Sweet getSweet(String name) {
 			for(Sweet sweet : sweetList) {
-				if(sweet.name.equals(name)) {
+				
+				if(sweet.name.equalsIgnoreCase(name)) {
 					return sweet;
 				}
 			}
 			return null;		 
-		}
-			
+		}	
 	}
